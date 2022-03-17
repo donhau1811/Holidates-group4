b7708636d35853bc86005c7087388b3fb7d10616
My Le <irisle412@gmail.com>
2022-03-17 13:26:47 +0700

    render Languages list

--- script.js
+++ script.js
@@ -1,50 +1,74 @@
 const API_KEY = "d06ac405-89a6-4e0a-beb3-800d37172570";
-//RENDER COUNTRIES LIST
-document.getElementById("countries-list-btn").addEventListener("click", () =>{
-    renderCountries();
-})
 
-const getCountries = async ()=>{
-    try{
-        const url = `https://holidayapi.com/v1/countries?pretty&key=${API_KEY}`
-        //here is how we add a dynamic value (API KEY) to the url
-        const res = await fetch(url)
-        const data = await res.json()
-        // console.log("data", data) //have a look the retrieved data
-        return data
-    } catch(err){
-        console.log("err", err)
-    }
-}
+const getCountries = async () => {
+  try {
+    const url = `https://holidayapi.com/v1/countries?pretty&key=${API_KEY}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log("data", data);
+    return data;
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+const renderCountries = async () => {
+  try {
+    const data = await getCountries();
+    const countriesList = document.getElementById("countries-list");
+    const ulCountriesList = countriesList.children[2];
+    ulCountriesList.innerHTML = "";
+    data.countries.forEach((country, index) => {
+      const x = document.createElement("li");
+      x.innerHTML = `<div class="bullet">${index + 1}</div>
+              <div class="li-wrapper">
+                  <div class="li-title">${country.name}</div>
+                  <div>Code: ${country.code}</div>
+              </div>`;
+      ulCountriesList.appendChild(x);
+    });
+  } catch (err) {
+    console.log("err", err);
+  }
+};
 
-const renderCountries = async()=>{
-    try{
-        //1. Fetch all the countries by using function `getCountries`
-        const data = await getCountries()
+document.getElementById("countries-list-btn").addEventListener("click", () => {
+  renderCountries();
+});
 
-        //2. Find the element with the id `countries-list`
-        const countriesList = document.getElementById("countries-list")
+// 1. render Language list
 
-        //3. Take out the `ul` element
-        const ulCountriesList = countriesList.children[2]
+const getLanguages = async () => {
+  try {
+    const url = `https://holidayapi.com/v1/languages?pretty&key=${API_KEY}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log("data", data);
+    return data;
+  } catch (err) {
+    console.log("err", err);
+  }
+};
 
-        //4. Delete the sample inside `ul` element
-        ulCountriesList.innerHTML=""
+const renderLanguages = async () => {
+  try {
+    const data = await getLanguages();
+    const languagesList = document.getElementById("languages-list");
+    const ulLanguagesList = languagesList.children[2];
+    ulLanguagesList.innerHTML = "";
+    data.languages.forEach((language, index) => {
+      const x = document.createElement("li");
+      x.innerHTML = `<div class="bullet">${index + 1}</div>
+              <div class="li-wrapper">
+                  <div class="li-title">${language.name}</div>
+                  <div>Code: ${language.code}</div>
+              </div>`;
+      ulLanguagesList.appendChild(x);
+    });
+  } catch (err) {
+    console.log("err", err);
+  }
+};
 
-        //5. Loop through the list of countries
-        data.countries.forEach((country, index)=>{
-            //Create new `li` for each element
-            const x = document.createElement("li")
-            x.innerHTML = `<div class="bullet">${index + 1}</div>
-            <div class="li-wrapper">
-                <div class="li-title">${country.name}</div>
-                <div>Code: ${country.code}</div>
-            </div>`;
-
-            //Then append them to the `ul` element
-            ulCountriesList.appendChild(x)
-        })
-    } catch(err){
-        console.log("err", err)
-    }
-}
+document
+  .querySelector("#languages-list-btn")
+  .addEventListener("click", renderLanguages);
